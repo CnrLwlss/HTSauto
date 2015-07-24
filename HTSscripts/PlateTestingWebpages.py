daa2baf436660e7e44fe1266c4d54fe2294b6638
Conor Lawless <cnr.lwlss@gmail.com>
2015-07-24 16:44:51 +0100

    More changes
    Optionally disable preview.  Build ideal image based on text file (.out
    file from Colonyzer in this case).

--- HTSscripts/PlateTestingWebpages.py
+++ HTSscripts/PlateTestingWebpages.py
@@ -1,7 +1,28 @@
 import PIL, os, math
 from PIL import Image
+import pandas
+import numpy
+
+def createPlateImage(outfile,density="Intensity"):
+    '''Read in file describing ideal plate and generate image.  outfile should be tab-delimited file containing Row, Column and density (e.g. Intensity by default) columns'''
+    ideal=pandas.read_csv(outfile,sep="\t")
+    Nrow=max(ideal.Row)
+    Ncol=max(ideal.Column)
+    arr=numpy.zeros((Nrow,Ncol),dtype=numpy.float)
+    dmax=max(ideal[density])
+    dmin=min(ideal[density])
+    ideal["DENS"]=ideal[density]-dmin
+    ideal["DENS"]=ideal["DENS"]/(dmax-dmin)
+    arr[ideal.Row-1,ideal.Column-1]=ideal.DENS
+    rgbarr=numpy.zeros((Nrow,Ncol,3),dtype=numpy.uint8)
+    rgbarr[...,0]=numpy.round(arr*256)
+    rgbarr[...,1]=numpy.round(arr*256)
+    rgbarr[...,2]=numpy.round(arr*256)
+    idealim=Image.fromarray(rgbarr,"RGB")
+    return(idealim)
  
 resizeWidth=500
+buildPreview=False
 
 #Get jpgs for which there are no corresponding pngs
 allfiles=os.listdir(os.getcwd())
@@ -13,38 +34,43 @@ pngfilenames=[os.path.splitext(f)[0]+".png" for f in newfilelist]
 
 #check to see if there are any new files, if not then ignore the rest, if there
 #are then resize and build a new preview file
-
+maxh=0
 if len(newfilelist)>0:
-    im0=Image.open(newfilelist[0])
-    w,h=im0.size
-    neww=resizeWidth
-    newh=int(round((float(neww)/float(w))*h))
     for jpg,png in zip(newfilelist,pngfilenames):
-		print jpg+" -> shrinking -> "+png
         im=Image.open(jpg)
-        small=im.resize((neww,newh),Image.ANTIALIAS)
+        w,h=im.size
+        newh=int(round((float(resizeWidth)/float(w))*h))
+        if newh>maxh:
+            maxh=newh
+        print jpg+" -> shrinking -> "+png
+        small=im.resize((resizeWidth,newh),Image.ANTIALIAS)
         small.save(png)
 
 # If we have already generated individual .pngs, probably better NOT to generate one big preview in this case
 # Especially if we are not going to build a massive image map
-Nimages=len(pngfilenames)
-bigim=Image.new("RGB",(2*neww,int(math.ceil(float(Nimages)/2.0))*newh))
+if buildPreview:
+    Nimages=len(pngfilenames)
+    bigim=Image.new("RGB",(2*resizeWidth,int(math.ceil(float(Nimages)/2.0))*maxh))
 
-row,col=0,0
-for imname in pngfilenames:
-    im=Image.open(imname)
-    bigim.paste(im,(col*neww,row*newh,(col+1)*neww,(row+1)*newh))
-    print row,col
-    col=col+1
-    if col>1:
-        col=0
-        row=row+1
-    bigim.save("Preview.png")
+    row,col=0,0
+    for imname in pngfilenames:
+        im=Image.open(imname)
+        bigim.paste(im,(col*resizeWidth,row*maxh,(col+1)*resizeWidth,(row+1)*maxh))
+        print row,col
+        col=col+1
+        if col>1:
+            col=0
+            row=row+1
+        bigim.save("Preview.png")
+
+if maxh>0:
+    idealim=createPlateImage("IdealPlate.out")
+    idealim=idealim.resize((resizeWidth,maxh),Image.NEAREST)
+    idealim.save("Ideal.PNG")
 
 # Start creating html files for building the image maps
 HTML='''<html>
 
-<<<<<<< HEAD
 <!doctype html>
 <html lang=en> 
 <head>
@@ -54,10 +80,6 @@ HTML='''<html>
 <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" type="text/css" href="CLstyle.css">
 </head>
-=======
-    <body>
-    <img src="Preview.png" alt="SGA Image data" />
->>>>>>> origin/master
 
 <body>
 <article id="main">
@@ -66,8 +88,11 @@ HTML='''<html>
 
 for jpg,png in zip(newfilelist,pngfilenames):
 	fname=os.path.splitext(png)[0]
-	HTML+='<h2>{}<h2>\n'.format(fname)
-	HTML+='<a href="{}"><img src="{}></a>\n'.format(jpg,png)
+	batchno=fname[1:7]
+	datetime=fname[-19:]
+	HTML+='<h2>{}<h2>\n'.format("Batch: "+batchno+" Date: "+datetime)
+	HTML+='<a href="{}"><img src="{}"></a>\n'.format(jpg,png)
+	HTML+='<a href="Ideal.png"><img src="Ideal.png"></a>\n'
 
 HTML+='''</article>
 </body>
